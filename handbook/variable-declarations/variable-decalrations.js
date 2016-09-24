6d7bdec539b9cc7d0fc265fff827b71eb128fa60
Almena <dsalmena+git@gmail.com>
2016-09-24 00:43:16 -0400

    variable declaration section of handbood

---
+++ handbook/variable-declarations/variable-decalrations.js
@@ -0,0 +1,20 @@
+function f(input) {
+    let a = 100;
+    if (input) {
+        // Still okay to reference 'a'
+        let b = a + 1;
+        return b;
+    }
+    // Error: 'b' doesn't exist here
+    //return b;
+}
+try {
+    throw "oh no!";
+}
+catch (e) {
+    console.log("Oh well.");
+}
+function h() {
+    a++; // illegal to use 'a' before it's declared;
+}
+let a;
---
+++ handbook/variable-declarations/variable-decalrations.ts
@@ -0,0 +1,25 @@
+﻿function f(input: boolean) {
+    let a = 100;
+
+    if (input) {
+        // Still okay to reference 'a'
+        let b = a + 1;
+        return b;
+    }
+
+
+    // Error: 'b' doesn't exist here
+    //return b;
+}
+
+try {
+    throw "oh no!";
+}
+catch (e) {
+    console.log("Oh well.");
+}
+
+function h() {
+    a++; // illegal to use 'a' before it's declared;
+}
+let a;
