c61be2ab0991f64df8352379d763e023a9d8aa89
Almena <dsalmena+git@gmail.com>
2016-09-29 00:40:42 -0400

    start handbook/generics using new example-runner

---
+++ handbook/generics/generics.ts
@@ -0,0 +1,36 @@
+﻿ExampleRunner.run(function (console) {
+    function identity(arg: number): number {
+        return arg;
+    }
+    console.log("identity: ", identity(13));
+
+    function identity2(arg: any): any {
+        return arg;
+    }
+    console.log("identity2: ", identity2("13"));
+
+    function identity3<T>(arg: T): T {
+        console.log("identity3: ",typeof arg);
+        return arg;
+    }
+    console.log("identity3.1: ", identity3(13.001));
+});
+
+ExampleRunner.run(function (console) {
+    function identity(arg: number): number {
+        return arg;
+    }
+    console.log("identity: ", identity(13));
+
+    function identity2(arg: any): any {
+        return arg;
+    }
+    console.log("identity2: ", identity2("13"));
+
+    function identity3<T>(arg: T): T {
+        console.log("identity3: ", typeof arg);
+        return arg;
+    }
+    console.log("identity3.1: ", identity3(13.001));
+    console.log("Test", console);
+});
