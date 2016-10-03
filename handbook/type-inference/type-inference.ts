caf3f7396005061e12df7cc2c15f6df5d9aa4d7b
Almena <dsalmena+git@gmail.com>
2016-10-02 21:51:17 -0400

    add handbook/type-inference

---
+++ handbook/type-inference/type-inference.ts
@@ -0,0 +1,38 @@
+﻿ExampleRunner.run(function (console) {
+    let x = 3;
+    let x2 = [0, 1, null];
+});
+
+ExampleRunner.run(function (console) {
+    window.onmousedown = function (mouseEvent) {
+        //console.log(mouseEvent.buton);  //<- Error
+    };
+    window.onmousedown = function (mouseEvent: any) {
+        console.log(mouseEvent.buton);  //<- Now, no error is given
+    };
+});
+
+ExampleRunner.run(function (console) {
+    interface Animal {
+        x: number;
+    }
+    class Elephant implements Animal {
+        nameElephant: string;
+        x: number;
+    }
+    class Rhino implements Animal {
+        x: number;
+        nameRhino: string;
+    }
+    class Snake implements Animal {
+        x: number;
+        name: string;
+    }
+    function createZoo(): Animal[] {
+        return [new Rhino(), new Elephant(),
+        new Snake()];
+    }
+
+    let x = createZoo();
+});
+
